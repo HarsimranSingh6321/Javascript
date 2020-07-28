0a05d486dd7dfbb9e4d93a772d24f3a299d4f36f
Harsimran Singh <harsimran.dastaarsirra@gmail.com>
2020-07-29 01:09:29 +0530

    Hold button bug solved

--- pig_dice_game/pig_dice.js
+++ pig_dice_game/pig_dice.js
@@ -2,6 +2,7 @@ var player1_score = 0;
 var player2_score = 0;
 var value = 0;
 var player_choice = 1;
+var count= 0
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.click1').disabled = true;
   document.querySelector('.hold').disabled = true;
@@ -12,43 +13,61 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.hold').disabled = false;
 
     document.querySelector('.turn').innerHTML = 'Player ' + player_choice + "'s turn ";
-    document.querySelector('.click1').onclick = function() {
 
-      var random = Math.floor(Math.random() * 6) + 1;
-      document.querySelector('.value').innerHTML = random;
-      if (random != 1) {
-        value = value + random;
-        var current_value = document.querySelector('.current_value').innerHTML;
-        current_value = "Temporary Score : " + value;
-        document.querySelector('.current_value').innerHTML = current_value;
-      } else if (random === 1) {
-        value = 0;
-        document.querySelector('.current_value').innerHTML = "Temporary Score : " + value;
-        if (player_choice == 1) {
-          player_choice = 2;
-          document.querySelector('.turn').innerHTML = 'Player  ' + player_choice + "'s turn ";
-        } else {
-          player_choice = 1;
-          document.querySelector('.turn').innerHTML = 'Player  ' + player_choice + "'s turn ";
+    document.querySelector('.click1').onclick = function() {
+      count=0;  // Clearing the (hold button click) counter
+
+      document.querySelector('.hold').disabled = false; //When die rolls a 1 hold button gets diabled and when player clicks roll dice button again then hold button gets enabled
+
+        var random = Math.floor(Math.random() * 6) + 1;
+        document.querySelector('.value').innerHTML = random;
+
+      //If a player rolls a die other than 1
+        if (random != 1) {
+          value = value + random;
+          var current_value = document.querySelector('.current_value').innerHTML;
+          current_value = "Temporary Score : " + value;
+          document.querySelector('.current_value').innerHTML = current_value;
+
+          //If die rolled by player gives 1
+        } else if (random === 1) {
+          value = 0;
+          document.querySelector('.hold').disabled = true; // When 1 is rolled disable hold button so that usin hold button the turns of the player doesn't change unnecessarily.
+          document.querySelector('.current_value').innerHTML = "Temporary Score : " + value;
+          if (player_choice == 1) {
+            player_choice = 2;
+            document.querySelector('.turn').innerHTML = 'Player  ' + player_choice + "'s turn ";
+          } else {
+            player_choice = 1;
+            document.querySelector('.turn').innerHTML = 'Player  ' + player_choice + "'s turn ";
+          }
         }
       }
-    }
-    document.querySelector('.hold').onclick = function() {
 
-      if (player_choice == 1) {
+    document.querySelector('.hold').onclick = function() {
+      count++;
+      if (count > 1){  //If a player clicks on hold button more than once consecutively
+        player_choice = player_choice;  //then the turn stays on that player only
+        document.querySelector('.turn').innerHTML = 'Player ' + player_choice + "'s turn ";
+
+      }
+
+      else if (player_choice == 1) {
         player1_score += value;
         document.querySelector('.player1').innerHTML = "Player 1's Score : " + player1_score;
         value = 0;
         document.querySelector('.current_value').innerHTML = "Temporary Score : " + value;
         player_choice = 2;
         document.querySelector('.turn').innerHTML = 'Player ' + player_choice + "'s turn ";
-      } else if (player_choice == 2) {
+      }
+      else if (player_choice == 2) {
         player2_score += value;
         document.querySelector('.player2').innerHTML = "Player 2's Score : " + player2_score;
         value = 0;
         document.querySelector('.current_value').innerHTML = "Temporary Score : " + value;
         player_choice = 1;
         document.querySelector('.turn').innerHTML = 'Player ' + player_choice + "'s turn ";
+
       }
       if (player1_score >= 100) {
         document.querySelector('#wins').innerHTML = "Hurray! player 1 wins.";
@@ -85,6 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
           player_choice = 1;
           document.querySelector('.turn').innerHTML = 'Player ' + player_choice + "'s turn ";
           document.querySelector('#wins').innerHTML = "Score 100, to win the game";
+
         }
       }
     }
